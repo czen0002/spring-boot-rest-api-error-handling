46bbd93cf263d55b6fe3fac7185398f06de45e02
czen0002 <zc114789@gmail.com>
2022-08-04 00:21:17 +1000

    [CHENG] Validate student id

--- src/main/java/io/czen/errorhandling/controller/StudentController.java
+++ src/main/java/io/czen/errorhandling/controller/StudentController.java
@@ -4,19 +4,25 @@ import io.czen.errorhandling.model.Student;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 
 @Controller
 @RequestMapping(value = "/v1")
+@Validated
 public class StudentController {
 
     @GetMapping(value = "/student")
-    public ResponseEntity<Student> getStudent(@RequestParam("studentId") Long studentId) {
+    public ResponseEntity<Student> getStudent(@RequestParam("studentId") @Min(1) @Max(12) Long studentId) {
         return new ResponseEntity<>(new Student(studentId, "Wayne", "Bruce"), HttpStatus.ACCEPTED);
     }
 
     @PostMapping(value = "/student", consumes = "application/json")
-    public ResponseEntity<Student> postStudent(@RequestBody Student student) {
+    public ResponseEntity<Student> postStudent(@RequestBody @Valid Student student) {
         return new ResponseEntity<>(new Student(student.getId(), student.getFirstName(), student.getLastName()), HttpStatus.CREATED);
     }
 }
