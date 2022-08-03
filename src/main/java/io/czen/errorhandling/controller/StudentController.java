75aa390b04eabdf4f906e0996bc1a733dc213831
czen0002 <zc114789@gmail.com>
2022-08-04 00:16:50 +1000

    [CHENG] Add post student endpoint

--- src/main/java/io/czen/errorhandling/controller/StudentController.java
+++ src/main/java/io/czen/errorhandling/controller/StudentController.java
@@ -1,11 +1,10 @@
 package io.czen.errorhandling.controller;
 
 import io.czen.errorhandling.model.Student;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping(value = "/v1")
@@ -13,6 +12,11 @@ public class StudentController {
 
     @GetMapping(value = "/student")
     public ResponseEntity<Student> getStudent(@RequestParam("studentId") Long studentId) {
-        return ResponseEntity.ok(new Student(studentId, "Wayne", "Bruce"));
+        return new ResponseEntity<>(new Student(studentId, "Wayne", "Bruce"), HttpStatus.ACCEPTED);
+    }
+
+    @PostMapping(value = "/student", consumes = "application/json")
+    public ResponseEntity<Student> postStudent(@RequestBody Student student) {
+        return new ResponseEntity<>(new Student(student.getId(), student.getFirstName(), student.getLastName()), HttpStatus.CREATED);
     }
 }
