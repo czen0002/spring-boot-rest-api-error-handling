a9fe39a8df7f4d0655ce430457fb0d1b68941f86
czen0002 <zc114789@gmail.com>
2022-08-07 20:10:32 +1000

    [CHENG] Update StudentController

--- src/main/java/io/czen/errorhandling/controller/StudentController.java
+++ src/main/java/io/czen/errorhandling/controller/StudentController.java
@@ -2,11 +2,11 @@ package io.czen.errorhandling.controller;
 
 import io.czen.errorhandling.model.Student;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,18 +15,20 @@ import javax.validation.Valid;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 
-@Controller
+@RestController
 @RequestMapping(value = "/v1")
 @Validated
 public class StudentController {
 
     @GetMapping(value = "/student")
-    public ResponseEntity<Student> getStudent(@RequestParam("studentId") @Min(1) @Max(12) Long studentId) {
-        return new ResponseEntity<>(new Student(studentId, "Wayne", "Bruce"), HttpStatus.ACCEPTED);
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public Student getStudent(@RequestParam("studentId") @Min(1) @Max(100) Long studentId) {
+        return new Student(studentId, "Wayne", "Bruce");
     }
 
     @PostMapping(value = "/student", consumes = "application/json")
-    public ResponseEntity<Student> postStudent(@RequestBody @Valid Student student) {
-        return new ResponseEntity<>(new Student(student.getId(), student.getFirstName(), student.getLastName()), HttpStatus.CREATED);
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student postStudent(@RequestBody @Valid Student student) {
+        return new Student(student.getId(), student.getFirstName(), student.getLastName());
     }
 }
