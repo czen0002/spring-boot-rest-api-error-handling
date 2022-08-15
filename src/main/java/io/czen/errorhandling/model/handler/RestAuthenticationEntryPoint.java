2e8802554d2611a2ddde58e3a8966e62e8f47504
czen0002 <zc114789@gmail.com>
2022-08-16 00:53:07 +1000

    [CHENG] authentication error return json response body

--- src/main/java/io/czen/errorhandling/model/handler/RestAuthenticationEntryPoint.java
+++ src/main/java/io/czen/errorhandling/model/handler/RestAuthenticationEntryPoint.java
@@ -3,6 +3,7 @@ package io.czen.errorhandling.model.handler;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.czen.errorhandling.model.error.ErrorBuilder;
 import io.czen.errorhandling.model.error.Errors;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -11,17 +12,19 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Collections;
 
 @Component
 public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
-    private ObjectMapper objectMapper = new ObjectMapper();
-
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        response.getOutputStream().println(objectMapper.writeValueAsString(new Errors().errorList(
-                Collections.singletonList(ErrorBuilder.buildUnauthorizedError(authException.getMessage())))));
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        Errors errors = new Errors().errorList(Collections.singletonList(ErrorBuilder.buildUnauthorizedError(authException.getMessage())));
+        OutputStream responseStream = response.getOutputStream();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.writeValue(responseStream, errors);
     }
 }
