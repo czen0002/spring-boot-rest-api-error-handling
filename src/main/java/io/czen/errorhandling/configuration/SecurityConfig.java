a8f27472db3f3316e2ee074827d3cd74db36b486
czen0002 <zc114789@gmail.com>
2022-08-15 23:31:06 +1000

    [CHENG] Update spring security

--- src/main/java/io/czen/errorhandling/configuration/SecurityConfig.java
+++ src/main/java/io/czen/errorhandling/configuration/SecurityConfig.java
@@ -1,13 +1,15 @@
 package io.czen.errorhandling.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception{
-        http.cors().and().csrf().disable();
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic();
+        return http.build();
     }
 }
