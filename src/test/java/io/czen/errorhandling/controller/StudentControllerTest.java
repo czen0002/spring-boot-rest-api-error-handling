8c6d67a66d89a0d48c20daaf41558abd6e47e53b
czen0002 <zc114789@gmail.com>
2022-08-15 23:44:11 +1000

    [CHENG] Update Controller test

--- src/test/java/io/czen/errorhandling/controller/StudentControllerTest.java
+++ src/test/java/io/czen/errorhandling/controller/StudentControllerTest.java
@@ -7,23 +7,52 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static javax.servlet.http.HttpServletResponse.SC_ACCEPTED;
+import static javax.servlet.http.HttpServletResponse.SC_BAD_REQUEST;
 
 @WebMvcTest
 public class StudentControllerTest {
 
     private static final String GET_STUDENT_PATH = "/v1/student";
+    private static final String BAD_REQUEST = "BAD_REQUEST";
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    @WithMockUser
+    @WithMockUser("USER")
     public void getStudentSuccessfully() throws Exception {
         String url = "/v1/student?studentId=10";
-        mockMvc.perform(MockMvcRequestBuilders.get(url)
-                .with(csrf())).andExpect(status().is(SC_ACCEPTED));
+        mockMvc.perform(MockMvcRequestBuilders.get(url))
+                .andExpect(status().is(SC_ACCEPTED))
+                .andExpect(jsonPath("$.id").value(10))
+                .andExpect(jsonPath("$.firstName").value("Wayne"))
+                .andExpect(jsonPath("$.lastName").value("Bruce"));
+    }
+
+    @Test
+    @WithMockUser("USER")
+    public void getStudentThrowMethodArgumentTypeMismatchException() throws Exception {
+        String url = "/v1/student?studentId=1.5";
+        mockMvc.perform(MockMvcRequestBuilders.get(url))
+                .andExpect(status().is(SC_BAD_REQUEST))
+                .andExpect(jsonPath("$.errorList[0].title").value(BAD_REQUEST))
+                .andExpect(jsonPath("$.errorList[0].code").value("400"))
+                .andExpect(jsonPath("$.errorList[0].detail").value("Parameter 'studentId' " +
+                        "is invalid. Expected a valid java.lang.Long, but received class java.lang.String."));
+    }
+
+    @Test
+    @WithMockUser("USER")
+    public void getStudentThrowConstraintViolationException() throws Exception {
+        String url = "/v1/student?studentId=0";
+        mockMvc.perform(MockMvcRequestBuilders.get(url))
+                .andExpect(status().is(SC_BAD_REQUEST))
+                .andExpect(jsonPath("$.errorList[0].title").value(BAD_REQUEST))
+                .andExpect(jsonPath("$.errorList[0].code").value("400"))
+                .andExpect(jsonPath("$.errorList[0].detail")
+                        .value("must be greater than or equal to 1"));
     }
 }
