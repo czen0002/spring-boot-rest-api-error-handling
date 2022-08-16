7512c5654d6b95cdc809ea95b2105fa8e9cb2343
czen0002 <zc114789@gmail.com>
2022-08-16 22:00:44 +1000

    [CHENG] add users in SecurityConfig

--- src/main/java/io/czen/errorhandling/configuration/SecurityConfig.java
+++ src/main/java/io/czen/errorhandling/configuration/SecurityConfig.java
@@ -5,7 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Arrays;
 
 @Configuration
 public class SecurityConfig {
@@ -23,5 +29,20 @@ public class SecurityConfig {
             .httpBasic()
             .authenticationEntryPoint(restAuthenticationEntryPoint);
         return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails apiUser = User
+                .withUsername("apiuser")
+                .password("{noop}password")
+                .roles("USER")
+                .build();
+        UserDetails admin = User
+                .withUsername("admin")
+                .password("{noop}password")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(Arrays.asList(apiUser, admin));
     }
 }
