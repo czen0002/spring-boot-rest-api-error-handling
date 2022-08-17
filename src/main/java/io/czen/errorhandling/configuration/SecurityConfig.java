520ddaba32c0464545df320466ae951cd759f8b5
czen0002 <zc114789@gmail.com>
2022-08-18 00:31:26 +1000

    [CHENG] Update SecurityConfig

--- src/main/java/io/czen/errorhandling/configuration/SecurityConfig.java
+++ src/main/java/io/czen/errorhandling/configuration/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -22,12 +23,14 @@ public class SecurityConfig {
             RestAuthenticationEntryPoint restAuthenticationEntryPoint,
             RestAccessDeniedHandler restAccessDeniedHandler) throws Exception {
         http
-            .httpBasic()
-            .authenticationEntryPoint(restAuthenticationEntryPoint)
-            .and()
+
+
             .authorizeRequests()
             .antMatchers(HttpMethod.POST, "/v1/student").hasRole("ADMIN")
             .and()
+                .httpBasic()
+                .authenticationEntryPoint(restAuthenticationEntryPoint)
+                .and()
             .exceptionHandling()
             .accessDeniedHandler(restAccessDeniedHandler)
             .and()
@@ -36,18 +39,26 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails apiUser = User
-                .withUsername("apiuser")
-                .password("{noop}password")
-                .roles("USER")
-                .build();
-        UserDetails admin = User
-                .withUsername("admin")
-                .password("{noop}password")
-                .roles("ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(apiUser, admin);
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("apiuser").password("{noop}password").roles("USER")
+                .and()
+                .withUser("admin").password("{noop}password").roles("USE", "ADMIN");
     }
+
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsService() {
+//        UserDetails apiUser = User
+//                .withUsername("apiuser")
+//                .password("{noop}password")
+//                .roles("USER")
+//                .build();
+//        UserDetails admin = User
+//                .withUsername("admin")
+//                .password("{noop}password")
+//                .roles("ADMIN")
+//                .build();
+//        return new InMemoryUserDetailsManager(apiUser, admin);
+//    }
 }
